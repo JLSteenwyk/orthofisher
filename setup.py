6e007c58669b87de50c8bb9d4ab2c2bb5e07a085
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2021-01-20 06:25:14 -0800

    version bump

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     packages=["orthofisher"],
     classifiers=CLASSIFIERS,
     entry_points={"console_scripts": ["orthofisher = orthofisher.orthofisher:main"]},
-    version="0.0.6",
+    version="0.0.7",
     include_package_data=True,
     install_requires=REQUIRES,
 )
